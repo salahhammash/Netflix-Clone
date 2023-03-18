fbf3c85a1e07573932b7396efe76ab64a834721e
SalahHammash <s.m.hammash@gmail.com>
2023-03-18 16:09:26 +0300

    finally done

--- src/component/FavList.js
+++ src/component/FavList.js
@@ -1,78 +1,54 @@
-import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import Modal from 'react-bootstrap/Modal';
+
+
+import React, { useState, useEffect } from 'react';
+
+import axios from 'axios';
+import Favmovie from './Favmovie';
+
+
 
 function FavList() {
 
-  const [updateIsDone, setUpdate] = useState(false)
-  const [show, setShow] = useState(false);
+  const [newData, setNewData] = useState([])
 
-  const handleClose = () => 
-  {setShow(false);
+  
+const newDATAfromDeleteReq = (arrData) =>{
+
+  setNewData(arrData)
+}
+
+
+
+
+
+
+  const art = async () => {
+    try {
+      const dataWait = await axios.get('https://movies-library-production-c3da.up.railway.app/getMovies')
+      //we took the data from server.js (from get data that inside tha DB)
+
+      const axiosData = await dataWait.data;
+      setNewData(axiosData)
+
+    }
+    catch (error) {
+      console.log(error.message);
+    }
   }
-  const handleShow = () => 
-  {setShow(true);
-  }
-
-  const showMore = () => {
- 
-
-  }
-
-  const UpdateItem = () => {
-
-    console.log("done");
-
-    setUpdate(true)
-  }
-  const DeletItem = () => {
-
-    console.log("deleted");
-  }
-
-
+  useEffect(() => {
+    art();
+  }, [])
   return (
     <div>
-
-
-<Button variant="primary" onClick={handleShow}>
-      <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-    </Button>
-
-
-
-    
-
-    <Modal show={show} onHide={handleClose}>
-      <Modal.Header closeButton>
-        <Modal.Title>Modal heading</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={handleClose}>
-          Close
-        </Button>
-       
-        <Button variant="success" onClick={UpdateItem}>Update</Button>{' '}
-        
-        <Button variant="danger" onClick={DeletItem}>Delete</Button>{' '}
-
-      </Modal.Footer>
-    </Modal>
-
+      {newData.map((ee) => {
+        return (
+          <Favmovie item={ee} key= {ee.id}  newFun= {newDATAfromDeleteReq}/>
+        )
+      })}
 
     </div >
+
+
   );
 }
 export default FavList
